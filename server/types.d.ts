c8b0c645ce3a8a0ff38b07f2b30edc245b4bb72b
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2021-08-04 13:28:16 -0400

    chore: Add a Type for Representing Sermon Info
    We may consider a better location for types in the future.

---
+++ server/types.d.ts
@@ -0,0 +1,11 @@
+/** Representation of sermon info */
+export interface SermonInfo {
+  speaker: { new: boolean; value: string; firstName: string; lastName: string };
+  title: string;
+  series: { new: boolean; value: string; newValue: string };
+  reference: { book: string; passage: string };
+  date: `${number}${number}${number}${number}-${number}${number}-${number}${number}`;
+  time: "Sunday Morning" | "Sunday Evening" | "Other";
+  sermonFileName: string;
+  sermonPicName: string;
+}
