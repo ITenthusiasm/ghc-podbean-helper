8b0a7a30ac925ca5cc0d52bff9a5d69fe5b023c1
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2021-08-06 09:34:59 -0400

    draft: Start Trying to Create a Way to Upload Sermons (INCOMPLETE)
    We've made a lot of headway trying to provide a means to upload
    sermons. However, we're running into a bug with the PodBean API
    or a really obscure syntax error on our end. And it isn't clear.
    We're in communication with the PodBean help team at the moment.
    Committing current progress so it doesn't get lost.
    
    Current Progress:
    * Create a route to validate the form data, update file info,
      and attempt to publish a new episode.
    * Create a means to upload files via `/files/uploadAuthorize`
      as mentioned in the docs.
    
    Remaining:
    * Need `podbeanService.upload` to successfully generate
      new episodes. We're getting an obscure error about
      the episode type right now, even though the type should
      be correct.

--- server/routes/podbeanRouter.ts
+++ server/routes/podbeanRouter.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { SermonFormDataValidator } from "../validators";
 import { podbeanService } from "../services";
+import { applyID3Tags, updateSermonFileName } from "../helpers/files";
+import { UserError } from "../models/errors";
+import type { SermonFormData, SermonDetails } from "../types";
 
 /** Router handling any Podbean API requests */
 const podbeanRouter = express.Router();
@@ -10,6 +14,35 @@ podbeanRouter.get("/token", async function (_, res, next) {
     const token = await podbeanService.getToken();
     res.json(token);
   } catch (err) {
+    next(err);
+  }
+});
+
+podbeanRouter.post("/upload", async function (req, res, next) {
+  try {
+    // Run validation
+    await SermonFormDataValidator.validate(req.body as SermonFormData);
+
+    // Map form data
+    const { speaker: speakerFormData, series: seriesFormData } = req.body as SermonFormData;
+    const { firstName, lastName } = speakerFormData;
+    const speaker = speakerFormData.new ? `${firstName} ${lastName}` : speakerFormData.value;
+    const series = seriesFormData.new ? seriesFormData.newValue : seriesFormData.value;
+    const sermonInfo: SermonDetails = Object.assign(req.body, { speaker, series });
+
+    // Update sermon file with proper info
+    await applyID3Tags(sermonInfo);
+    const newSermonFileName = await updateSermonFileName(sermonInfo);
+
+    // Publish episode
+    const url = await podbeanService.publish({ ...sermonInfo, sermonFileName: newSermonFileName });
+    res.json(url);
+  } catch (err) {
+    if (err instanceof UserError) {
+      res.status(400).json(err);
+      return;
+    }
+
     next(err);
   }
 });
--- server/services/podbeanService.ts
+++ server/services/podbeanService.ts
@@ -1,6 +1,9 @@
 /* eslint-disable camelcase */
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import path from "path";
+import { promises as fs } from "fs";
 import { PodbeanError } from "../models/errors";
+import type { SermonDetails } from "../types";
 
 const podbeanApi = axios.create({ baseURL: process.env.BASE_PODBEAN_URL });
 
@@ -34,4 +37,97 @@ async function getToken(): Promise<string> {
   }
 }
 
-export default { getToken };
+// Private
+async function getFileKey(filename: string): Promise<string> {
+  const access_token = await getToken();
+
+  const [, ext] = filename.split(".");
+  const content_type = ext === "mp3" ? ("audio/mp3" as const) : ("image/png" as const);
+
+  const filepath = path.resolve(process.env.SERMON_FILES_DIR as string, filename);
+  const { size: filesize } = await fs.stat(filepath);
+
+  try {
+    const { data } = await podbeanApi.get("files/uploadAuthorize", {
+      params: { access_token, filename, filesize, content_type },
+    });
+
+    return data.file_key as string;
+  } catch (err) {
+    throw new PodbeanError(err.response.data);
+  }
+}
+
+/**
+ * Publishes a new sermon using the provided info
+ * @param sermonInfo
+ * @returns The permalink URL of the sermon on Podbean
+ */
+async function publish(sermonInfo: SermonDetails): Promise<string> {
+  const { speaker, title, reference, time, sermonFileName, sermonPicName } = sermonInfo;
+  const access_token = await getToken();
+  const media_key = await getFileKey(sermonFileName);
+  const logo_key = await getFileKey(sermonPicName);
+  console.log("Media Key: ", media_key);
+  console.log("Logo Key: ", logo_key);
+
+  // Prepare data for Podbean to display
+  const podcastTitle = `${reference.book} ${reference.passage}: ${title} (${speaker})`;
+  const podcastDescription = (() => {
+    const quotedTitle = title && `"${title}"`;
+
+    const eventAt = (() => {
+      if (time === "Sunday Morning") return "Sunday morning gathering of";
+      if (time === "Sunday Evening") return "Sunday evening gathering of";
+      return "message at";
+    })();
+
+    const untrimmedDescription = `${speaker} preaches ${quotedTitle} from ${reference.book} ${reference.passage} in this ${eventAt} Grace Harbor Church.`;
+    return untrimmedDescription.trim().replace(/\s{2,}/g, " ");
+  })();
+
+  try {
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+
+    const data = {
+      access_token,
+      type: "public",
+      title: podcastTitle,
+      content: podcastDescription,
+      status: "publish",
+      media_key,
+      logo_key,
+    };
+
+    const axiosConfig: AxiosRequestConfig = {
+      headers,
+      params: data,
+    };
+
+    // Try to verify that we're getting the right episode type
+    const { data: getPodcast } = await podbeanApi.post("podcasts", null, {
+      headers,
+      params: { access_token },
+    });
+    console.dir(getPodcast, { depth: 5 });
+    const episodeType = getPodcast.podcasts[0].allow_episode_type[0];
+    console.log("Episode Type: ", episodeType);
+
+    // Try to publish episode
+    axiosConfig.params.type = episodeType;
+    const { data: episode } = await podbeanApi.post("episodes", null, axiosConfig);
+
+    // Look at results
+    console.log(episode.permalink_url);
+    return episode.permalink_url as string;
+  } catch (err) {
+    console.error(err);
+    throw new PodbeanError(err.response.data);
+  }
+}
+
+// TODO: Stop exposing `getToken` after testing is complete and it is no longer needed
+const podbeanService = { getToken, publish };
+export default podbeanService;
