5bff668d9e6f79fbf0625e7a37503bb5df0ef2be
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2021-09-03 13:27:27 -0400

    refactor: Change `test-data` to `json-data` with JSON Files
    These JSON files will actually be used as our "database"
    moving forward.

---
+++ server/json-data/index.ts
@@ -0,0 +1,3 @@
+export { default as series } from "./series.json";
+export { default as speakers } from "./speakers.json";
+export { default as bibleBooks } from "./bibleBooks.json";
--- server/test-data/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as series } from "./series";
-export { default as speakers } from "./speakers";
-export { default as bibleBooks } from "./bibleBooks";
